d3d3d16e100137d15ae83c2d64a5961721fdc447
kioke takaaki <taka.vao.321@gmail.com>
2015-09-15 22:00:45 +0900

    バッチファイルを作成
    ワンクリックでスケジューラ登録を実現
    未テスト

---
+++ bats/regist.bat
@@ -0,0 +1,40 @@
+@echo off
+rem タスクスケジューラにタスクを登録するバッチファイル
+rem バッチファイルの場所にlogin.xml, send.vbsが必要です
+rem 作者 marshmallow-warriors/ryokun
+
+set name=%USERNAME%
+set current_dir=%CD%
+set root_dir=%HOMEDRIVE%\marsh
+set script_dir=%root_dir%\%name%
+set script_name=test.vbs
+
+
+echo タスクにスクリプトを登録します
+echo パスワードの入力を2回求められる可能性があります
+echo 注意:管理者権限で実行してください
+
+pause
+
+If NOT EXIST %current_dir%\login.xml (
+	echo login.xmlが存在しません
+	pause
+	exit
+)
+If NOT EXIST %current_dir%\send.vbs (
+	echo send.vbsが存在しません
+	pause
+	exit
+)
+
+If NOT EXIST %root_dir% mkdir %root_dir%
+If NOT EXIST %root_dir%\%name% mkdir %root_dir%\%name%
+
+copy %current_dir%\%script_name% %script_dir%
+
+schtasks /create /tn login_%name% /ru %name% /xml %current_dir%\login.xml
+schtasks /change /tn login_%name% /tr %script_dir%\%script_name%
+
+echo すべての作業が終了しました
+
+pause
